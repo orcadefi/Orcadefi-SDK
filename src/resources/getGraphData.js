3219378a152d68aa51a86e4bd88f2ff4f034f796
orcadefi <dev@orcadefi.com>
2020-11-13 23:25:23 -0600

    Update getGraphData.js

--- src/resources/getGraphData.js
+++ src/resources/getGraphData.js
@@ -1,14 +1,115 @@
 var axios = require('axios');
 
-var config = {
-  method: 'get',
-  url: 'http://orcadefi.com/api/v1/public/all',
-};
+function getAaveLendGraph(){
+  var config = {
+    method: 'get',
+    url: 'http://orcadefi.com/api/v1/public/graoh/lend/aave',
+  };
 
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+  axios(config)
+  .then(function (response) {
+    return JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+function getAaveBorrowGraph(){
+  var config = {
+    method: 'get',
+    url: 'http://orcadefi.com/api/v1/public/graoh/borrow/aave',
+  };
+
+  axios(config)
+  .then(function (response) {
+    return JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+function getCompoundBorrowGraph(){
+  var config = {
+    method: 'get',
+    url: 'http://orcadefi.com/api/v1/public/graoh/borrow/compound',
+  };
+
+  axios(config)
+  .then(function (response) {
+    return JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+function getCompoundLendGraph(){
+  var config = {
+    method: 'get',
+    url: 'http://orcadefi.com/api/v1/public/graoh/lend/compound',
+  };
+
+  axios(config)
+  .then(function (response) {
+    return JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+function getDydxBorrowGraph(){
+  var config = {
+    method: 'get',
+    url: 'http://orcadefi.com/api/v1/public/graoh/borrow/dydx',
+  };
+
+  axios(config)
+  .then(function (response) {
+    return JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+function getDydxLendGraph(){
+  var config = {
+    method: 'get',
+    url: 'http://orcadefi.com/api/v1/public/graoh/lend/dydx',
+  };
+
+  axios(config)
+  .then(function (response) {
+    return JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+function getFulcrumLendGraph(){
+  var config = {
+    method: 'get',
+    url: 'http://orcadefi.com/api/v1/public/graoh/lend/fulcrum',
+  };
+
+  axios(config)
+  .then(function (response) {
+    return JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+function getFulcrumBorrowGraph(){
+  var config = {
+    method: 'get',
+    url: 'http://orcadefi.com/api/v1/public/graoh/borrow/fulcrum',
+  };
+
+  axios(config)
+  .then(function (response) {
+    return JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
